0f9c7158fe077f208461119769ac35266ce9b511
Sophie <sophiesokol123@gmail.com>
2025-02-07 11:46:44 +0000

    Animate numbers on calculation using react-spring

--- src/components/CalculatedAge.tsx
+++ src/components/CalculatedAge.tsx
@@ -1,8 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { intervalToDuration } from "date-fns";
+import { useSpring, animated } from "react-spring";
 
 interface AgeDisplayProps {
   birthdate: Date | null;
+}
+
+interface NumberAnimationProps {
+  n: number,
+  delay: number
+}
+
+const NumberAnimation: React.FC<NumberAnimationProps> = ({ n, delay }) => {
+  const {number} = useSpring({
+    from: {number: 0},
+    number: n,
+    delay: delay,
+    config: {mass: 1, tension: 20, friction: 10}
+  });
+  return <animated.div className="inline-block">{number.to((n) => n.toFixed(0))}</animated.div>
 }
 
 const CalculatedAge: React.FC<AgeDisplayProps> = ({ birthdate }) => {
@@ -40,9 +56,38 @@ const CalculatedAge: React.FC<AgeDisplayProps> = ({ birthdate }) => {
   
   return (
     <div className='flex flex-col pt-3 pl-1'>
-        <p className='ageText md:pb-4'><span className='text-purple'>{years}</span> years</p>
-        <p className='ageText md:pb-4'><span className='text-purple'>{months}</span> months</p>
-        <p className='ageText md:pb-2'><span className='text-purple'>{days}</span> days</p>
+        <p className='ageText md:pb-4'>
+            {birthdate !== null ? (
+              <span className='text-purple'>
+                <NumberAnimation n={Number(years)} delay={400} />
+              </span> 
+            ) : (
+              <span className='text-purple'>
+                {years}
+              </span>
+            )} years</p>
+
+         <p className='ageText md:pb-4'>
+            {birthdate !== null ? (
+              <span className='text-purple'>
+                <NumberAnimation n={Number(months)} delay={800} />
+              </span> 
+            ) : (
+              <span className='text-purple'>
+                {months}
+              </span>
+            )} months</p>
+
+<p className='ageText md:pb-4'>
+            {birthdate !== null ? (
+              <span className='text-purple'>
+                <NumberAnimation n={Number(days)} delay={1500} />
+              </span> 
+            ) : (
+              <span className='text-purple'>
+                {days}
+              </span>
+            )} days</p>
     </div>
   )
 }
